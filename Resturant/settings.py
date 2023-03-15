f3afbc1b49a0fb9918275867aabdf49c34cc3277
alibekshak <alibekshakiro1@gmail.com>
2023-03-15 21:32:35 +0600

    update settings

--- Resturant/settings.py
+++ Resturant/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t%b#q0_w9x$8w!zzy-ik#y*7u2po4h$4&5mp(z4yl4o&oth1w_"
+SECRET_KEY = os.getenv("SECRET_KEY_Resturant")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
